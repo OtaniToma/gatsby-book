d85100df2c471dc8a5dbe11da2357e58e63d85b9
Toma Otani <otani.toma@gmail.com>
2020-06-26 16:11:01 -0700

    add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from '../components/layout'
+
+export default () => (
+  <Layout>
+    <h1 style={{ padding: "50vh 0", textAlign: "center" }}>404 Page Not Found</h1>
+  </Layout>
+)
