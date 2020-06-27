fac4a0dea93a2cebee0194fc7e8eb794ba1c8174
Toma Otani <otani.toma@gmail.com>
2020-06-27 12:58:33 -0700

    show images and icons on blog template

--- src/pages/blogpost.js
+++ src/pages/blogpost.js
@@ -1,51 +1,80 @@
 import React from 'react'
+import { graqhql, graphql } from "gatsby"
+import Img from 'gatsby-image'
 import Layout from '../components/layout'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClock, faFolderOpen } from '@fortawesome/free-regular-svg-icons'
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
-export default () => (
+export default ({ data }) => (
   <Layout>
     <div className="eyecatch">
-          <figure>
-            <img src="images-baseblog/eyecatch.jpg" alt="アイキャッチ画像の説明" />
-          </figure>
-        </div>
+      <figure>
+        <Img
+          fluid={data.contentfulBlogPost.eyecatch.fluid}
+          alt={data.contentfulBlogPost.eyecatch.description}
+        />
+      </figure>
+    </div>
     <article className="content">
-          <div className="container">
-            <h1 className="bar">記事のタイトル</h1>
-            <aside className="info">
-              <time dateTime="XXXX-XX-XX"><FontAwesomeIcon icon={faClock} />XXXX年XX月XX日</time>
-              <div className="cat">
-                <FontAwesomeIcon icon={faFolderOpen} />
-                <ul>
-                  <li className="スラッグ">カテゴリーＡ</li>
-                  <li className="スラッグ">カテゴリーＢ</li>
-                </ul>
-              </div>
-            </aside>
-            <div className="postbody">
-              <p>
-                記事の本文です。記事の本文です。記事の本文です。記事の本文です。記事の本文です。
-                記事の本文です。記事の本文です。記事の本文です。記事の本文です。記事の本文です。
-                記事の本文です。記事の本文です。記事の本文です。記事の本文です。記事の本文です。
-              </p>
-            </div>
-            <ul className="postlink">
-              <li className="prev">
-                <a href="base-blogpost.html" rel="prev">
-                  <FontAwesomeIcon icon={faChevronLeft} />
-                  <span>前の記事</span>
-                </a>
-              </li>
-              <li className="next">
-                <a href="base-blogpost.html" rel="next">
-                  <span>次の記事</span>
-                  <FontAwesomeIcon icon={faChevronRight} />
-                </a>
-              </li>
+      <div className="container">
+        <h1 className="bar">{data.contentfulBlogPost.title}</h1>
+        <aside className="info">
+          <time dateTime="{data.contentfulBlogPost.publishDate}"><FontAwesomeIcon icon={faClock} />{data.contentfulBlogPost.publishDateJP}</time>
+          <div className="cat">
+            <FontAwesomeIcon icon={faFolderOpen} />
+            <ul>
+              {data.contentfulBlogPost.category.map(cat => (
+                <li className={cat.categorySlug} key={cat.id}>
+                  {cat.category}
+                </li>
+              ))}
             </ul>
           </div>
-        </article>
+        </aside>
+        <div className="postbody">
+          <p>
+            記事の本文です。記事の本文です。記事の本文です。記事の本文です。記事の本文です。
+            記事の本文です。記事の本文です。記事の本文です。記事の本文です。記事の本文です。
+            記事の本文です。記事の本文です。記事の本文です。記事の本文です。記事の本文です。
+              </p>
+        </div>
+        <ul className="postlink">
+          <li className="prev">
+            <a href="base-blogpost.html" rel="prev">
+              <FontAwesomeIcon icon={faChevronLeft} />
+              <span>前の記事</span>
+            </a>
+          </li>
+          <li className="next">
+            <a href="base-blogpost.html" rel="next">
+              <span>次の記事</span>
+              <FontAwesomeIcon icon={faChevronRight} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </article>
   </Layout>
 )
+
+export const query = graphql`
+  query MyQuery {
+    contentfulBlogPost {
+      title
+      publishDateJP:publishDate(formatString: "YYYY年MM月DD日")
+      publishDate
+      category {
+        category
+        categorySlug
+        id
+      }
+      eyecatch {
+        fluid(maxWidth: 1600) {
+          ...GatsbyContentfulFluid_withWebp
+        },
+        description
+      }
+    }
+  }
+`
